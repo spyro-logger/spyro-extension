97b7af4be90520c76c7a282040883ff16e7b6ea3
Rao Pachva <Rao.Pachva@cerner.com>
2020-04-29 11:04:58 -0500

    Changes to consume spyro server

--- src/main/clients/JiraClient.js
+++ src/main/clients/JiraClient.js
@@ -1,12 +1,14 @@
-import JiraApi from 'jira-client';
+import axios from 'axios';
 
 /**
  * Create a new JIRA issue.
  * For more information, see the link provided below:
  * https://developer.atlassian.com/cloud/jira/platform/rest/v3/#api-rest-api-3-issue-post)
  *
+ * @param {String} spyroServerBaseUrl
+ *    The base url for spyro server.
  * @param {String} jiraHost
- *    The host to connect to for the jira instance. Ex: jira.somehost.com
+ *    The host to connect for the jira instance. Ex: jira.somehost.com
  * @param {Object} jiraCredential
  *    The jira credential object consisting of user name and password to authenticate requests.
  * @param {Object} fieldValues
@@ -14,20 +16,15 @@ import JiraApi from 'jira-client';
  *    For more information refer to fields section in the above mentioned JIRA document.
  * @returns {Object} The response data for the created issue.
  */
-const createIssue = async (jiraHost, jiraCredential, fieldValues) => {
+const createIssue = async (spyroServerBaseUrl, jiraHost, jiraCredential, fieldValues) => {
   try {
-    const jira = new JiraApi({
-      protocol: 'https',
-      host: jiraHost,
-      username: jiraCredential.username,
-      password: jiraCredential.password,
-      apiVersion: '2',
-      strictSSL: true,
-    });
+    const addNewIssueUrl = `${spyroServerBaseUrl}/jira/issue`;
+    const basicAuth = `Basic ${btoa(`${jiraCredential.username}:${jiraCredential.password}`)}`;
+    const headers = { 'Content-Type': 'application/json', Authorization: basicAuth };
 
-    const issueJSON = { fields: fieldValues };
-    const createIssueResponse = await jira.addNewIssue(issueJSON);
-    return createIssueResponse;
+    const payLoadData = JSON.stringify({ jiraHost, fieldValues });
+
+    return await axios.post(addNewIssueUrl, payLoadData, { headers });
   } catch (error) {
     const errorMessage = `Error while creating JIRA issue: ${error}`;
     // eslint-disable-next-line no-console
--- src/main/clients/SplunkClient.js
+++ src/main/clients/SplunkClient.js
@@ -1,100 +1,35 @@
 import axios from 'axios';
-import moment from 'moment';
 
-const getNodeValueByNodeName = (parentNode, nodeName) => {
-  const nodes = parentNode.getElementsByTagNameNS('*', 'key');
-  // Looping over HTMLCollection, which doesn't have typical array functions
-  for (let i = 0; i < nodes.length; i += 1) {
-    if (nodes[i].getAttribute('name') === nodeName) {
-      return nodes[i].childNodes[0].nodeValue;
-    }
-  }
+const splunkJobDetailsRetriever = () => ({ spyroServerBaseUrl, splunkAPIURL, splunkApp, searchId, credential }) => {
+  const retrieveJobDetailsUrl = `${spyroServerBaseUrl}/splunk/jobDetails?splunkAPIURL=${splunkAPIURL}&splunkApp=${splunkApp}&searchId=${searchId}`;
 
-  return undefined;
-};
-
-const getFormattedDateTime = (xmlDoc, nodeName) => {
-  const startDateTime = getNodeValueByNodeName(xmlDoc, nodeName);
-  return moment(startDateTime).format('MM/DD/YY h:mm:ss A');
-};
-
-const populateSearchRange = (xmlDoc) => {
-  return `${getFormattedDateTime(xmlDoc, 'earliestTime')} to ${getFormattedDateTime(xmlDoc, 'latestTime')}`;
-};
-
-const populateRequiredJobDetails = (jobBySIDResponse, jobFirstEventResponse, jobSummaryResponse) => {
-  const jobDetails = {};
-
-  const jobFields = jobSummaryResponse.data.fields;
-
-  // eslint-disable-next-line no-underscore-dangle
-  jobDetails.stackTrace = jobFirstEventResponse.data.results[0]._raw;
-  const xmlDoc = new DOMParser().parseFromString(jobBySIDResponse.data, 'text/xml');
-  jobDetails.occurences = getNodeValueByNodeName(xmlDoc, 'eventCount');
-  jobDetails.searchRange = populateSearchRange(xmlDoc);
-  jobDetails.searchString = getNodeValueByNodeName(xmlDoc, 'custom.search');
-  jobDetails.fields = [...Object.entries(jobFields)]
-    .map(([key, value]) => {
-      return {
-        key,
-        value: value.modes.map((mode) => `${mode.value} (${mode.count})`).join(', '),
-      };
-    })
-    .reduce((accumulator, entry) => {
-      return {
-        ...accumulator,
-        [entry.key]: entry.value,
-      };
-    }, []);
-
-  return jobDetails;
-};
-
-const splunkJobDetailsRetriever = () => ({ splunkAPIURL, splunkApp, searchId, credential }) => {
-  const retrieveJobBySIDUrl = `${splunkAPIURL}/nobody/${splunkApp}/search/jobs/${searchId}`;
-  const retrieveJobSummaryUrl = `${retrieveJobBySIDUrl}/summary?output_mode=json`;
-  const retrieveJobFirstEventUrl = `${splunkAPIURL}/nobody/${splunkApp}/search/jobs/${searchId}/events?count=1&f=_raw&output_mode=json`;
-
-  const auth = { username: credential.username, password: credential.password };
-
-  return new Promise((resolve, reject) => {
-    axios
-      .all([
-        axios.get(retrieveJobBySIDUrl, {
-          auth,
-        }),
-        axios.get(retrieveJobFirstEventUrl, {
-          auth,
-        }),
-        axios.get(retrieveJobSummaryUrl, {
-          auth,
-        }),
-      ])
-      .then(
-        axios.spread((jobBySIDResponse, jobFirstEventResponse, jobSummary) => {
-          const populatedJobDetails = populateRequiredJobDetails(jobBySIDResponse, jobFirstEventResponse, jobSummary);
-          resolve(populatedJobDetails);
-        }),
-      )
-      .catch((error) => {
-        const errorMessage = `Error while retrieving splunk job details: ${error}`;
-        // eslint-disable-next-line no-console
-        console.error(errorMessage);
-        reject(new Error(errorMessage));
-      });
+  const basicAuth = `Basic ${btoa(`${credential.username}:${credential.password}`)}`;
+  const headers = { 'Content-Type': 'application/x-www-form-urlencoded', Authorization: basicAuth };
+  return axios.get(retrieveJobDetailsUrl, { headers }).catch((error) => {
+    const errorMessage = `Error while retrieving splunk job details: ${error}`;
+    // eslint-disable-next-line no-console
+    console.error(errorMessage);
+    throw new Error(error);
   });
 };
 
-const createEventType = async (credential, splunkAPIURL, splunkApp, jiraIdentifier, searchString) => {
+const createEventType = async (
+  spyroServerBaseUrl,
+  credential,
+  splunkAPIURL,
+  splunkApp,
+  jiraIdentifier,
+  searchString,
+) => {
   try {
-    const createEventTypeUrl = `${splunkAPIURL}/${credential.username}/${splunkApp}/saved/eventtypes`;
+    const createEventTypeUrl = `${spyroServerBaseUrl}/splunk/event`;
     const basicAuth = `Basic ${btoa(`${credential.username}:${credential.password}`)}`;
+    const formData = `splunkAPIURL=${splunkAPIURL}&searchString=${searchString}&splunkApp=${splunkApp}&jiraIdentifier=${jiraIdentifier}`;
 
-    const formData = `name=${encodeURIComponent(jiraIdentifier)}&priority=5&disabled=0&description=${encodeURIComponent(
-      jiraIdentifier,
-    )}&search=${searchString}`;
-
-    const headers = { 'Content-type': 'application/x-www-form-urlencoded', Authorization: basicAuth };
+    const headers = {
+      'Content-type': 'application/x-www-form-urlencoded',
+      Authorization: basicAuth,
+    };
 
     return await axios.post(createEventTypeUrl, formData, { headers });
   } catch (error) {
@@ -105,15 +40,13 @@ const createEventType = async (credential, splunkAPIURL, splunkApp, jiraIdentifi
   }
 };
 
-const updateEventTypePermission = async (credential, splunkAPIURL, splunkApp, jiraIdentifier) => {
+const updateEventTypePermission = async (spyroServerBaseUrl, credential, splunkAPIURL, splunkApp, jiraIdentifier) => {
   try {
-    const updateEventTypeUrl = `${splunkAPIURL}/${credential.username}/${splunkApp}/saved/eventtypes/${jiraIdentifier}/acl`;
+    const updateEventTypeUrl = `${spyroServerBaseUrl}/splunk/event/permission`;
 
     const basicAuth = `Basic ${btoa(`${credential.username}:${credential.password}`)}`;
 
-    const formData = `perms.read=${encodeURIComponent('*')}&perms.write=${encodeURIComponent(
-      '*',
-    )}&sharing=app&owner=${encodeURIComponent(credential.username)}`;
+    const formData = `splunkAPIURL=${splunkAPIURL}&splunkApp=${splunkApp}&jiraIdentifier=${jiraIdentifier}&owner=${credential.username}`;
 
     const headers = { 'Content-Type': 'application/x-www-form-urlencoded', Authorization: basicAuth };
 
